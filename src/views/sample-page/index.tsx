73d1066ec2fe2f243edd52c3704a29305aa75c66
Ivana <i.alvarez@rsegrp.com>
2021-10-28 15:43:22 -0400

    skeleton

---
+++ src/views/sample-page/index.tsx
@@ -0,0 +1,17 @@
+// material-ui
+import { Typography } from '@material-ui/core';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+
+// ==============================|| SAMPLE PAGE ||============================== //
+
+const SamplePage = () => (
+    <MainCard title="Pendientes">
+        <Typography variant="body2">
+            Lorem ipsum dolor sit amen,consenter nipissing eli,sed do elusion tempos incident ut laborers et doolie magna alissa 
+        </Typography>
+    </MainCard>
+);
+
+export default SamplePage;
